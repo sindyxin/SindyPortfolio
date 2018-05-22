c8288298323852d50978ada7243eb2a2912eb6f1
Sindy <sindyxin1@gmail.com>
2018-05-21 23:10:58 -0700

    Create seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create(
+    title: "My Blog Post #{blog}",
+    body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  )
+end
+puts "10 blog created"
+
+5.times do |skill|
+  Skill.create(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+    
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio|
+  Portfolio.create(
+    title: "Portfolio title #{portfolio}",
+    subtitle: "My great project",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+puts "9 portfolio items created"
