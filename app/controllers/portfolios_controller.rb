106e19a75ac4e4a53d0ad94f9547f7a766351c50
Sindy <sindyxin1@gmail.com>
2018-05-22 16:28:02 -0700

    Add edit and update functionality and button of edit and new

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,7 +18,26 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def edit 
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html {
+          redirect_to portfolios_path, notice: "Update successfully"
+        }
+      else
+        format.html { render :edit }
+      end
+    end 
+  end
 end
+
+
+  
+
 
 
 private
