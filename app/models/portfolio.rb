8f51120542f16f4ef54389afca5a0825f0bd5186
Sindy <sindyxin1@gmail.com>
2018-05-24 18:20:04 -0700

    implemented image default values setting

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,17 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
-  #custom scope
+    #custom scope
   def self.angular
     where(subtitle: 'Angular')
   end
 
   scope :My_great_project_subtitle, -> { where(subtitle: 'My great project') }
+
+  after_initialize :set_default
+  def set_default
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
+
+
