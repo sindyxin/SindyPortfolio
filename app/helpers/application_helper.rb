a3de21652879629276650fe10921b4986d331c5c
Sindy <sindyxin1@gmail.com>
2018-05-30 00:08:27 -0700

    Update master layout style with helper method and add footer partial component

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
 
-  def login_helper
+  # if we want add class for style , we can give a argument in method
+  def login_helper(style)
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) + "<br>".html_safe+
-      (link_to 'Login', new_user_session_path)
+      (link_to 'Register', new_user_registration_path, class: style) + " ".html_safe+
+      (link_to 'Login', new_user_session_path, class: style)
     else 
-      link_to 'Logout', destroy_user_session_path, method: :delete 
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
   
