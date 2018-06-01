5e195c4478f2575b1ca593fdb37f68ac296ebcc6
Sindy <sindyxin1@gmail.com>
2018-05-31 20:42:32 -0700

    Implemented partials for blog. font awesome, and pagination

--- Gemfile
+++ Gemfile
@@ -61,3 +61,5 @@ gem 'jquery-rails'
 
 gem 'xin_copyright', '~> 0.1.0'
 gem 'petergate', '~> 1.8', '>= 1.8.1'
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.4'
+gem 'kaminari', '~> 1.1', '>= 1.1.1'
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,8 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
+    #@blogs = Blog.all
   end
 
   # GET /blogs/1
