bd36fe20f4bb31fb34a68203544725a397d9d87d
Sindy <sindyxin1@gmail.com>
2018-06-17 23:23:49 -0700

    Refactored alert into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -92,6 +92,14 @@ module ApplicationHelper
   #     end
   #   end
   # end
-
+  def alert
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+  def alert_generator msg
+    js add_gritter(msg, title: "Xin Li Portfolio", sticky: false)
+  end
 
 end
