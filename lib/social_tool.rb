866d994cd6474768769bd0e879a706919bed93a4
Sindy <sindyxin1@gmail.com>
2018-06-19 02:04:57 -0700

    Finish styles for tech news page

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -7,7 +7,7 @@ module SocialTool
       config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
     end
 
-    client.search("#technology", result_type: 'recent').take(6).collect do |tweet|
+    client.search("#coding#developer", result_type: 'recent').take(6).collect do |tweet|
       "#{tweet.user.screen_name}: #{tweet.text}"
     end
   end
