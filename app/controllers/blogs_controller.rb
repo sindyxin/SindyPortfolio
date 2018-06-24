9e5ef08eed68d2215d2ef6271afb6824427cd8be
Sindy <sindyxin1@gmail.com>
2018-06-24 00:38:57 -0700

    Implemented redirect and Authority for blog show page when non admin tries to access draft blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,10 +16,15 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
+    end
+
   end
 
   # GET /blogs/new
@@ -74,6 +79,7 @@ class BlogsController < ApplicationController
   def toggle_status
     if @blog.draft?
       @blog.published! 
+      @blog.update(created_at: Time.now)
     elsif @blog.published?
       @blog.draft! 
     end
