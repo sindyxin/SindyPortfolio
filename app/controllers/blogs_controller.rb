8473e938a5a015dbd14cd209f9a8e9911b91919f
Sindy <sindyxin1@gmail.com>
2018-06-29 03:06:57 -0700

    Implemented style changes for home about and contact pages

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,7 @@ class BlogsController < ApplicationController
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
   # GET /blogs
   # GET /blogs.json
+
   def index
     if logged_in?(:site_admin)
       @blogs = Blog.page(params[:page]).per(5).order(created_at: :desc)
